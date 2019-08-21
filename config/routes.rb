2c4ec1ced06c71f7b8262c28d0ff69472451caa9
unionups <unionups@gmail.com>
2019-08-21 03:11:46 +0300

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-	namespace :api do
-		namespace :v1 do
-			post '/start_verification', to: 'sessions#new'
+  namespace :api do
+    namespace :v1 do
+      # unauthenticated routes
+      post '/start_verification', to: 'sessions#new'
       post '/confirm_verification', to: 'sessions#create'
-			constraints Constraints::UserConstraint do
-
-			end
-			constraints Constraints::AdminConstraint do
-				scope module: 'admin' do
-				 
-				end
-			end
-		end
-	end
+      constraints Constraints::UserConstraint do
+        # authenticated :user role routes
+      end
+      constraints Constraints::AdminConstraint do
+        scope module: 'admin' do
+          # authenticated :admin role routes
+         
+        end
+      end
+    end
+  end
 end
